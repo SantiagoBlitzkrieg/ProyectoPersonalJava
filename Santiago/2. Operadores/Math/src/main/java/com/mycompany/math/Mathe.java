f32199dce38d88f6f7c7b9d145d6f94c0f096379
SantiagoBlitzkrieg <sgomezcam@unal.edu.co>
2023-09-30 18:44:37 -0500

    Avance 1 clase 2

---
+++ Santiago/2. Operadores/Math/src/main/java/com/mycompany/math/Mathe.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.math;
+import java.util.Scanner;
+/**
+ *
+ * @author gomez
+ */
+public class Mathe {
+
+    public static void main(String[] args) {
+        double raiz = Math.sqrt(9); // Raiz cuadrada
+        int raiz2 = (int)Math.sqrt(9); //convertimos el double del sqrt a entero
+        double base =5, exponente= 2;
+        double respuesta = Math.pow(base, exponente); //exponencial
+        
+        
+        
+        
+        System.out.println(raiz);
+        System.out.println(raiz2);
+        System.out.println(respuesta);
+    }
+
+}
